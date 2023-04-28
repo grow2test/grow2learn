7c4abfd687a54dada82892a092bd8a8429bd5b81
yash joshi <yjoshi.2098@gmail.com>
2023-04-28 15:37:11 -0400

    added login githublogin and home views

---
+++ src/components/GithubLogin.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="github-login">
+        <button @click="login">Login with GitHub</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "GithubLogin",
+    methods: {
+        login() {
+            const clientId = "YOUR_CLIENT_ID";
+            const redirectUri = "http://localhost:8080/login/callback";
+            const url = `https://github.com/login/oauth/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&scope=user`;
+            window.location.href = url;
+        }
+    }
+};
+</script>
+
+<style>
+.github-login {
+    max-width: 400px;
+    margin: 0 auto;
+    padding: 20px;
+    font-family: sans-serif;
+    text-align: center;
+}
+
+button {
+    padding: 10px;
+    font-size: 16px;
+    border-radius: 5px;
+    border: none;
+    background-color: #007bff;
+    color: #fff;
+    cursor: pointer;
+    transition: all 0.2s ease;
+}
+
+button:hover {
+    background-color: #0062cc;
+}
+</style>
---
+++ src/components/LoginView.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="login">
+        <h1>Login</h1>
+        <GithubLogin />
+    </div>
+</template>
+
+<script>
+import GithubLogin from "@/components/GithubLogin.vue";
+
+export default {
+    name: "LoginView",
+    components: {
+        GithubLogin
+    }
+};
+</script>
+
+<style>
+.login {
+    max-width: 400px;
+    margin: 0 auto;
+    padding: 20px;
+    font-family: sans-serif;
+    text-align: center;
+}
+
+h1 {
+    font-size: 48px;
+    margin-bottom: 20px;
+}
+</style>
