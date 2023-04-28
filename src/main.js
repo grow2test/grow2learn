a9c88ea46c156dec5a6a684f7ace213754439314
yash joshi <yjoshi.2098@gmail.com>
2023-04-28 15:30:44 -0400

    vue 2.0 boilerplate for grow2learn

--- src/main.js
+++ src/main.js
@@ -1,8 +1,22 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import App from "./App.vue";
 
-Vue.config.productionTip = false
+Vue.use(VueRouter);
+
+import Home from "./components/HomeView.vue";
+import Login from "./components/LoginView.vue";
+
+const router = new VueRouter({
+  mode: "history",
+  base: __dirname,
+  routes: [
+    { path: "/", component: Home },
+    { path: "/login", component: Login },
+  ],
+});
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  router,
+  render: (h) => h(App),
+}).$mount("#app");
