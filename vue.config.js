dca9dc13866dd1c72c59b75689a5f7da8759c334
yash joshi <yjoshi.2098@gmail.com>
2023-04-28 16:35:29 -0400

    added package to remove cors error for github apis

--- vue.config.js
+++ vue.config.js
@@ -2,4 +2,16 @@ const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
   runtimeCompiler: true,
+  devServer: {
+    proxy: {
+      "/github": {
+        target: "https://github.com",
+        changeOrigin: true,
+        pathRewrite: {
+          "^/github": "",
+        },
+        logLevel: "debug",
+      },
+    },
+  },
 });
